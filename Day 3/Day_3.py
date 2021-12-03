83344810779e2823dec5e7eacfdf73514a332a61
Olli <werthwein@fzi.de>
2021-12-03 12:01:13 +0100

    solution part 1 for day 3

--- Day 3/Day_3.py
+++ Day 3/Day_3.py
@@ -19,13 +19,39 @@ puzzle = Puzzle(year=2021, day=3)
 # Solution Part 1
 # --------------------------------
 input = puzzle.input_data.split("\n")
-print(input[:10])
 
-#print("Solution Part 1: ", horizontal*depth)
+length = len(input)
+line_length = len(input[0])
+
+final_bit = [0] * line_length
+gamma_rate = [None] * line_length
+epsilon_rate = [None] * line_length
+
+for line in input:
+    line = [int(i) for i in line] # cast all elements to integers
+
+    for index, value in enumerate(line):
+        final_bit[index] += value # add each bit to final_bit list
+
+for index, bit in enumerate(final_bit):
+    if bit - length // 2 > 0: # check if there are more 1 than 0
+        gamma_rate[index] = 1
+        epsilon_rate[index] = 0
+    else: 
+        gamma_rate[index] = 0
+        epsilon_rate[index] = 1
+
+# binary to decimal
+gamma_rate_int = int("".join(str(i) for i in gamma_rate),2)
+epsilon_rate_int = int("".join(str(i) for i in epsilon_rate),2)
+
+print(gamma_rate, gamma_rate_int)
+print(epsilon_rate, epsilon_rate_int)
+print("Solution Part 1: ", epsilon_rate_int*gamma_rate_int)
 
 # --------------------------------
 # Solution Part 2
 # --------------------------------
 
 
-#rint("Solution Part 2: ", horizontal*depth)
+#print("Solution Part 2: ", solution)
