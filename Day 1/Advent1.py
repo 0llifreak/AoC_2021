86c69357e2d117834ab7519aa2e2140af6643f09
lar1far1 <95473158+lar1far1@users.noreply.github.com>
2021-12-03 10:36:00 +0100

    Add files via upload

---
+++ Day 1/Advent1.py
@@ -0,0 +1,9 @@
+#Advent1
+with open('input.txt') as f:
+  report = [int(i) for i in f]
+counter= 0
+for i in range(len(report)-1):
+    j=i+1
+    if report[j] > report[i]:
+        counter= counter+1
+print(counter)
