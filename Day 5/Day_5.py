ceab71e4c46c0862e3275987f8b2aba20f91cf26
0lli <werthwein@fzi.de>
2021-12-04 22:51:11 +0100

    Add Day 5

---
+++ Day 5/Day_5.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# -- Advent of Code 2021 ---------
+#
+# Puzzle 5
+#
+# Author: Olli
+# --------------------------------
+
+from aocd.models import Puzzle
+
+# --------------------------------
+# Get input data from AoC
+# --------------------------------
+puzzle = Puzzle(year=2021, day=5)
+
+# --------------------------------
+# Solution Part 1
+# --------------------------------
+input = puzzle.input_data.split("\n")
+print(input[:10])
+
+#print("Solution Part 1: ", horizontal*depth)
+
+# --------------------------------
+# Solution Part 2
+# --------------------------------
+
+
+#rint("Solution Part 2: ", horizontal*depth)
