68590c732b526e23bf54691f9d6b3f8e88fc0b1a
0lli <74310296+0llifreak@users.noreply.github.com>
2021-12-06 18:07:12 +0100

    Add Day 7

---
+++ Day 7/Day_7.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# -- Advent of Code 2021 ---------
+#
+# Puzzle 7
+#
+# Author: Olli
+# --------------------------------
+
+from aocd.models import Puzzle
+
+# --------------------------------
+# Get input data from AoC
+# --------------------------------
+puzzle = Puzzle(year=2021, day=7)
+
+
+# --------------------------------
+# Solution Part 1
+# --------------------------------
+input = puzzle.input_data.split(",")
+input = [int(i) for i in input]
+
+
+solution = 0
+print("Solution Part 1: ", solution)
+
+
+
+
+# --------------------------------
+# Solution Part 2
+# --------------------------------
+input = puzzle.input_data.split(",")
+input = [int(i) for i in input]
+
+
+solution = 0
+print("Solution Part 2: ", solution)
