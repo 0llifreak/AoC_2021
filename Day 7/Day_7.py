4f705752c91be26f6b7d4be3160147d01b526190
0lli <74310296+0llifreak@users.noreply.github.com>
2021-12-07 09:07:38 +0100

    Add Part 1 for Day 7

--- Day 7/Day_7.py
+++ Day 7/Day_7.py
@@ -9,12 +9,12 @@
 # --------------------------------
 
 from aocd.models import Puzzle
+import numpy as np
 
 # --------------------------------
 # Get input data from AoC
 # --------------------------------
 puzzle = Puzzle(year=2021, day=7)
-
 
 # --------------------------------
 # Solution Part 1
@@ -22,11 +22,16 @@ puzzle = Puzzle(year=2021, day=7)
 input = puzzle.input_data.split(",")
 input = [int(i) for i in input]
 
+input_np = np.array(input)
+cost_list = []
 
-solution = 0
+unique_input = set(input) # nice way to remove duplicate values ;-)
+
+for pos in unique_input:
+    cost_list.append(sum(abs(input_np - pos))) 
+
+solution = min(cost_list)
 print("Solution Part 1: ", solution)
-
-
 
 
 # --------------------------------
