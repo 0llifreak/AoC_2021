a34c881253d056da25330823f945598e4ad4061e
0lli <74310296+0llifreak@users.noreply.github.com>
2021-12-10 21:16:10 +0100

    Solution for Part 1

--- Day 8/Day_8.py
+++ Day 8/Day_8.py
@@ -18,13 +18,23 @@ puzzle = Puzzle(year=2021, day=8)
 # --------------------------------
 # Solution Part 1
 # --------------------------------
-input = puzzle.input_data.split(",")
-input = [int(i) for i in input]
+input = puzzle.input_data.splitlines()
 
+input_filtered = []
 
+for line in input:
+    input_filtered.append(line.split(" | "))
+# [[vorne, hinten],[vorne, hinten], ...]
+#   ------1------   ------2------
 
-solution = 0
-print("Solution Part 1: ", solution)
+# Count all of length 2,3,4 and 7
+counter = 0
+for line in input_filtered:
+    for digit in line[1].split():   # digits im hinteren Teil als array
+        if len(digit) >= 2 and len(digit) <= 4 or len(digit) == 7:
+            counter += 1
+
+print("Solution Part 1: ", counter)
 
 
 
@@ -32,7 +42,7 @@ print("Solution Part 1: ", solution)
 # Solution Part 2
 # --------------------------------
 input = puzzle.input_data.split(",")
-input = [int(i) for i in input]
+#input = [int(i) for i in input]
 
 
 solution = 0
